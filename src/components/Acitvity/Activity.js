c6762d303b622adf05edc00ef82e6458b652ae0f
habib610 <habib54562@gmail.com>
2020-10-06 12:42:39 +0600

    delete update

--- src/components/Acitvity/Activity.js
+++ src/components/Acitvity/Activity.js
@@ -20,14 +20,14 @@ const Activity = (props) => {
     console.log(e.currentTarget.name)
     const uniqueKey = e.currentTarget.name;
     console.log(typeof uniqueKey)
-    // fetch('http://localhost:4200/delete/' + uniqueKey, {
-    //     method: 'DELETE',
-    //     headers: {
-    //         'Content-type': 'application/json'
-    //        },
-    //   })
-    //   .then(res => res.json()) // or res.json()
-    //   .then(res => console.log("deleted successfully"))
+    fetch(`http://localhost:5000/delete/${uniqueKey}`, {
+        method: 'DELETE',
+        headers: {
+            'Content-type': 'application/json'
+           },
+      })
+      .then(res => res.json()) 
+      .then(res => console.log("deleted successfully"))
     
 
 
